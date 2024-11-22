e9caae5bb7b48f271c6c7d4ba68e87fd7c28e0e6
Douglas Santiago <douglasfssantiago@gmail.com>
2024-11-22 16:47:20 -0600

    js updating2 img

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,15 @@ dropdownBtn.addEventListener('click', function() {
     dropdownContent.classList.toggle('show');
 });
 
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}

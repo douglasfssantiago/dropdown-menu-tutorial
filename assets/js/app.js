2b7ed6c13d70e2a772e36078b777ba20b6c0f7c2
Douglas Santiago <douglasfssantiago@gmail.com>
2024-11-22 16:31:43 -0600

    js update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,13 @@
 'use strict';
 
+// Get the dropdown button
+const dropdownBtn = document.querySelector('.dropdown-btn');
+// Get the dropdown content
+const dropdownContent = document.querySelector('.dropdown-content');
+
+// Add a click event to the dropdown button
+dropdownBtn.addEventListener('click', function() {
+    // Toggle the class that shows or hides the dropdown content
+    dropdownContent.classList.toggle('show');
+});
+
